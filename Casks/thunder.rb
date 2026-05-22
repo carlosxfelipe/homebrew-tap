653b53249be6f575065c56883f904d0872fb0d4b
Carlos Felipe Araujo <carlosxfelipe@gmail.com>
2026-05-22 08:33:26 -0300

    chore: bump thunder cask to version 1.8.0

--- Casks/thunder.rb
+++ Casks/thunder.rb
@@ -1,8 +1,8 @@
 cask "thunder" do
-  version "1.7.0"
-  sha256 "3e2b05d36baf5fe6a1befbc225e8f86fd151258c00ba16feea8ba3c914c20d61"
+  version "1.8.0"
+  sha256 "9a9f688c8eaa7d75a10b6115b829f3e1c9549166db3a3f1bd5ebaea069370d5a"
 
-  # Tratamento dinâmico: o tag é v1.7.0, mas o DMG de release segue a versão do Xcode (1.7) com build (1).
+  # Tratamento dinâmico: o tag é v1.8.0, mas o DMG de release segue a versão do Xcode (1.8) com build (1).
   url "https://github.com/carlosxfelipe/thunder/releases/download/v#{version}/Thunder-#{version.major_minor}-1.dmg"
   name "Thunder"
   desc "File manager written in Swift with SwiftUI"
