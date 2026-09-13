26c563490a35feb68a095b88ea06893b0301e129
Carlos Felipe Araujo <carlosxfelipe@gmail.com>
2026-09-13 00:12:50 -0300

    update live-wallpaper to 1.1.0

--- Casks/live-wallpaper.rb
+++ Casks/live-wallpaper.rb
@@ -1,8 +1,8 @@
 cask "live-wallpaper" do
-  version "1.0.2"
-  sha256 "e9657f14039020df488dc2729afcc32f1f0116b7858432338321d4ecfd8767e7"
+  version "1.1.0"
+  sha256 "1a37fc9e9ca7b303328e2b9719096a3cbaf39766652943f018723e22d6800a22"
 
-  url "https://github.com/carlosxfelipe/live-wallpaper/releases/download/v#{version}/LiveWallpaper-#{version}-5.dmg"
+  url "https://github.com/carlosxfelipe/live-wallpaper/releases/download/v#{version}/LiveWallpaper-1.1-6.dmg"
   name "LiveWallpaper"
   desc "Lightweight, native macOS menu bar app to set custom gradients and looping videos as your desktop wallpaper"
   homepage "https://github.com/carlosxfelipe/live-wallpaper"
