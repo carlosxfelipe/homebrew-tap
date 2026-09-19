896d0e53e107a0deb673bf194fd6109ad8dae561
Carlos Felipe Araujo <carlosxfelipe@gmail.com>
2026-09-19 10:49:38 -0300

    feat: update video-downloader to version 1.1.0

--- Casks/video-downloader.rb
+++ Casks/video-downloader.rb
@@ -1,8 +1,8 @@
 cask "video-downloader" do
-  version "1.0.0"
-  sha256 "a41121a53294d33b5d5db032428b9e385bf9b0dae676eb7ce5617f132bc987b8"
+  version "1.1.0"
+  sha256 "48625d90732fdaeab344c2146fe3ad489619881a8da22ad60abddeea4a0bdc71"
 
-  url "https://github.com/carlosxfelipe/video-downloader/releases/download/v#{version}/VideoDownloader-1.0-1.dmg"
+  url "https://github.com/carlosxfelipe/video-downloader/releases/download/v#{version}/VideoDownloader-1.1-2.dmg"
   name "VideoDownloader"
   desc "Native macOS GUI for yt-dlp to download videos and audio from YouTube and hundreds of other sites"
   homepage "https://github.com/carlosxfelipe/video-downloader"
